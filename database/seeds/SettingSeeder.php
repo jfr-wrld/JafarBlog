75e15d09dd14931408f0a76eb9e0e971fa218ccb
jfr-wrld <asidik.a.j@mail.ugm.ac.id>
2021-12-13 10:16:50 +0700

    hello

--- database/seeds/SettingSeeder.php
+++ database/seeds/SettingSeeder.php
@@ -13,12 +13,12 @@ class SettingSeeder extends Seeder
     public function run()
     {
         $setting = new Setting;
-        $setting->owner = 'Rifki';
-        $setting->bio = "Hi, I'm Rifki. I live in Muara Badak, East Borneo, Indonesia. I'm Web Developer with PHP (Laravel or CodeIgniter) and Android Developer with Kotlin. You can see my portfolio in my Github";
-        $setting->web_portfolio = 'https://github.com/RifkiCS29';
-        $setting->fb = 'https://www.facebook.com/rifki.jackson.12';
-        $setting->twitter = 'https://twitter.com/Rifki_CS29';
-        $setting->instagram = 'https://www.instagram.com/rifki_cs29/';
+        $setting->owner = 'Asidik Al Jafar';
+        $setting->bio = "Hi, I'm Asidik Al Jafar. UI/UX Design Enthusiasts";
+        $setting->web_portfolio = 'https://github.com/jfr-wrld';
+        $setting->fb = 'https://web.facebook.com/iamjfrr';
+        $setting->twitter = 'https://twitter.com/iamjfrr';
+        $setting->instagram = 'https://www.instagram.com/iam.jfr/';
 
         $setting->save();
 
